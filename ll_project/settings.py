ca381fde364a4d8ae19c4c5080a66588c2e711f1
AYu1130 <2963799545@qq.com>
2024-12-25 00:41:50 +0800

    导出第三方模块和修改收集静态资源配置

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nl%*#bq$!r!4#j3#30-l_bpmcm1$0_z-at)z1%hgawki-(*cl-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
